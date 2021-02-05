4880ffd836796918227fbabe62e7284866d03c35
cristik <zhenghua.ni@zhangmen.com>
2021-02-05 10:47:03 +0800

    nacos config demo

--- spring-cloud-config-samples/spring-cloud-sample-config-nacos/src/main/java/com/cristik/nacos/controller/TestController.java
+++ spring-cloud-config-samples/spring-cloud-sample-config-nacos/src/main/java/com/cristik/nacos/controller/TestController.java
@@ -1,6 +1,9 @@
 package com.cristik.nacos.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,17 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author zhenghua.ni
  */
+@RefreshScope
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
-    @NacosValue(value = "${aaa.bbb}", autoRefreshed = true)
-    private String test;
+    @Value("${test:{}}")
+    private User user;
 
 
     @GetMapping(value = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String test() {
-        return "";
+    public User test() {
+        JSON.parseObject("{}", User.class);
+        return user;
     }
+
+    public static class User {
+        private String name;
+        private Integer age;
+    }
+
 
 }
---
+++ spring-cloud-config-samples/spring-cloud-sample-config-nacos/src/test/java/com/Test.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Long aa = 0L;
+        System.out.println(aa == 0L);
+    }
+
+}
