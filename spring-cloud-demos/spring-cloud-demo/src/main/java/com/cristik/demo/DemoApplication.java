5d6574f46e52067847a08d91ef17c976b2857460
cristik <zhenghua.ni@zhangmen.com>
2021-02-08 14:29:34 +0800

    update

--- spring-cloud-demos/spring-cloud-demo/src/main/java/com/cristik/demo/DemoApplication.java
+++ spring-cloud-demos/spring-cloud-demo/src/main/java/com/cristik/demo/DemoApplication.java
@@ -12,19 +12,4 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-    public static class User {
-        private String aa;
-        private String bb;
-
-        public User() {
-        }
-
-        public User(String aa, String bb) {
-            this.aa = aa;
-            this.bb = bb;
-        }
-    }
-
-
 }
